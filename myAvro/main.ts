34ee1ed06fedd4b19f1ca62070259d987d226535
Abdullah-git-hub <arafat200423@gmail.com>
2024-07-30 19:38:33 +0600

    asdsd

--- myAvro/main.ts
+++ myAvro/main.ts
@@ -3,28 +3,33 @@ import Environment from "./runtime/environment.ts";
 import { evaluate } from "./runtime/interpreter.ts";
 import { MK_BOOL, MK_NULL, MK_NUM } from "./runtime/values.ts";
 
-const parser = new Parser();
-const env = new Environment();
-// env.declareVar("ক", MK_NUM(120), true);
-// env.declareVar("খ", MK_NUM(10), true);
-env.declareVar("ঠিক", MK_BOOL(true), true);
-env.declareVar("ভুল", MK_BOOL(false), true);
-env.declareVar("কিছুনা", MK_NULL(), true);
+async function run(filename: string) {
+    const parser = new Parser();
+    const env = new Environment();
+    env.declareVar("ঠিক", MK_BOOL(true), true);
+    env.declareVar("ভুল", MK_BOOL(false), true);
+    env.declareVar("কিছুনা", MK_NULL(), true);
 
-// INITIALIZE REPL
-console.log("\nAvroRepl v1.0");
-
-// Continue Repl Until User Stops Or Types `exit`
-while (true) {
-    const input = prompt("> ") || "ক";
-    // Check for no user input or exit keyword.
-    if (!input || input.includes("exit")) {
-        Deno.exit(1);
-    }
-
-    // Produce AST From sourc-code
-    const program = parser.produceAST(input?.toString());
-
+    const input = await Deno.readTextFile(filename);
+    const program = parser.produceAST(input.toString());
     const result = evaluate(program, env);
     console.log(result);
 }
+
+// INITIALIZE REPL
+// console.log("\nAvroRepl v1.0");
+
+// Continue Repl Until User Stops Or Types `exit`
+// while (true) {
+//     const input = prompt("> ") || "ক";
+//     if (!input || input.includes("exit")) {
+//         Deno.exit(1);
+//     }
+
+//     const program = parser.produceAST(input?.toString());
+
+//     const result = evaluate(program, env);
+//     console.log(result);
+// }
+
+run("/myAvro/mycode.txt");
